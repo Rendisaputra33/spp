3458d98240d4aa393f94a5cf38bafce8ddbd1563
Rendisaputra33 <68989984+Rendisaputra33@users.noreply.github.com>
2023-06-21 09:14:08 +0700

    Delete spp directory

--- spp/src/main/java/org/tugas/contract/Controller.java
+++
@@ -1,5 +0,0 @@
-package org.tugas.contract;
-
-public interface Controller {
-    void registerEvents();
-}
--- spp/src/main/java/org/tugas/contract/Dao.java
+++
@@ -1,9 +0,0 @@
-package org.tugas.contract;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
-public interface Dao<T> {
-    void toList(List<T> list, ResultSet result) throws SQLException;
-}
